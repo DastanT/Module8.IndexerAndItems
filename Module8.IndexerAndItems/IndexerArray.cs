2454b6d807f34c6df6b91baaf9d17d41cf51c0b9
Dastan <dastan.smarti@gmail.com>
2021-03-28 14:28:12 +0600

    Add project files.

---
+++ Module8.IndexerAndItems/IndexerArray.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module8.IndexerAndItems
+{
+    class IndexerArray
+    {
+        int[] arr;
+        public int length;
+        int result;
+        public IndexerArray(int size)
+        {
+            arr = new int[size];
+            length = size;
+        }
+
+        public bool ErrorOperation;
+        public int this[int index]
+        {
+            get
+            {
+                if (ok(index))
+                {
+                    ErrorOperation = false;
+                    return arr[index];
+                }
+                else
+                {
+                    ErrorOperation = true;
+                    return 0;
+                }
+            }
+            set
+            {
+                if (ok(index))
+                {
+                    arr[index] = value;
+                    ErrorOperation = false;
+                }
+                else
+                {
+                    ErrorOperation = true;
+                }
+            }
+        }
+
+        private int resultInSquare
+        {
+            get
+            {
+                return result*result;
+            }
+            set
+            {
+                result = value;
+            }
+        }
+
+        public void ArrayInSquare()
+        {
+            for (int i = 0; i < length; i++)
+            {
+                resultInSquare = arr[i];
+                Console.WriteLine("array " + "[" + i + "]" + " = " + resultInSquare);
+            }            
+        }
+
+        //Возвратить логическое значение true, если
+        //индекс находится в установленных границах
+        private bool ok(int index)
+        {
+            if (index >= 0 && index < length) return true;
+            return false;
+        }
+    }
+
+}
---
+++ Module8.IndexerAndItems/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module8.IndexerAndItems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Console.Write("Введите размер массива: ");
+            int size = int.Parse(Console.ReadLine());
+            IndexerArray indexerArray = new IndexerArray(size);
+            Console.WriteLine("Размер массива = " + indexerArray.length);
+            for (int i = 0; i < indexerArray.length; i++)
+            {
+                indexerArray[i] = rnd.Next(1, 20);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Весь массив: ");
+            for (int i = 0; i < indexerArray.length; i++)
+            {
+                Console.WriteLine("array " + "[" + i + "]" + " = " + indexerArray[i]);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Элемент массива в степени 2:");
+            indexerArray.ArrayInSquare();
+            Console.WriteLine();
+
+            Console.WriteLine("Обращаемся к массиву по индексу.");
+            Console.WriteLine("array [" + 0 +"] = " + indexerArray[0]);
+            Console.WriteLine("array [" + 1 + "] = " + indexerArray[1]);
+            Console.WriteLine("array [" + 2 + "] = " + indexerArray[2]);
+            Console.WriteLine("array [" + 3 + "] = " + indexerArray[3]);
+
+
+        }
+    }
+}
